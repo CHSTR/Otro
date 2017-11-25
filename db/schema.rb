7d834f06a6281795c21e56812ae72c389fd88922
JGRamirezG <ramirezguillermo266@gmail.com>
2017-11-25 13:32:41 -0300

    Arreglo de links

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20171123005518) do
-=======
-ActiveRecord::Schema.define(version: 20171122234918) do
->>>>>>> 0ebf00f3f3b347ddb56cf7952d1d1cc3edd4e611
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
