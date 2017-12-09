98fed9edcb15d5ad35a950791d80dba58e888e08
CHSTR <chstears@gmail.com>
2017-12-09 20:38:35 -0300

    Se agrega imagen predeterminada, y se mejora lo visual.

--- app/models/ckeditor/asset.rb
+++ app/models/ckeditor/asset.rb
@@ -1,4 +1,5 @@
 class Ckeditor::Asset < ActiveRecord::Base
   include Ckeditor::Orm::ActiveRecord::AssetBase
   include Ckeditor::Backend::Paperclip
+  #has_attached_file :media, :styles => {:thumb => "100x100>"}, :default_url => ""
 end
