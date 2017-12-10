4481a1bda127fa075b8f3d0ce779f1a6f4f1415c
CHSTR <chstears@gmail.com>
2017-12-10 16:17:48 -0300

    Solo el admin puede crear otros usuarios.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,17 @@
 class RegistrationsController < Devise::RegistrationsController
+  before_action :authenticate_user!, :redirect_unless_admin, only: [:new,:create]
+  skip_before_action :require_no_authentication
+  
   private
+
+  def redirect_unless_admin
+  	unless current_user.try(:admin?)
+  		flash[:error] = "Solo el admin puede hacer esto."
+  		redirect_to root_path
+  	else
+  		flash[:error] = "Solo el admin puede hacer esto."
+  	end
+  end
 
   def sign_up_params
     params.require(:user).permit(:email, :role_id, :password, :password_confirmation)
