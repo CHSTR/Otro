08da08c3f7b4059586b8c43adcf4da17ac3880b7
JGRamirezG <ramirezguillermo266@gmail.com>
2017-12-12 11:34:57 -0300

    cambios minimos

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,3 +13,5 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.precompile += %w( ckeditor/*)
+
+Rails.application.config.active_record.belongs_to_required_by_default = false
