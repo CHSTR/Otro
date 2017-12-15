4b242efa62ef27dc338d4cb5bb75d2b353c8e1d6
JGRamirezG <ramirezguillermo266@gmail.com>
2017-12-14 22:01:25 -0300

    Archivos se abren en otra pestaña

--- db/migrate/20171205214028_add_nombre_rut_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddNombreRutToUsers < ActiveRecord::Migration[5.1]
-  def change
-    add_column :users, :nombre, :string
-    add_column :users, :rut, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171214165452) do
+ActiveRecord::Schema.define(version: 20171215005207) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -370,6 +370,10 @@ ActiveRecord::Schema.define(version: 20171214165452) do
     t.string "link"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "document_file_name"
+    t.string "document_content_type"
+    t.integer "document_file_size"
+    t.datetime "document_updated_at"
   end
 
   create_table "roles", force: :cascade do |t|
